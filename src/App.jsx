8806dbc5267e66f5894856dae086eb3f8180c5a1
Ankush171 <ankushkumar1412005@gmail.com>
2024-05-28 17:30:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import TodoList from './component/TodoList';
 // import Ticket from './component/Ticket';
 
 function App() {
-  // let winningCond = (ticket)=>{
-  //   return ticket[0]===0;
-  // }
+  let winningCond = (ticket)=>{
+    return ticket[0]===0;
+  }
   // we pass funcrtion as a prop ;
   return (
-  // <>
-  // {/* <Ticketnum num={5} />
-  // <Ticketnum num={4} />
-  // <Ticketnum num={3} /> */}
-  // {/* <Ticket  ticket={[0,2,1]}/>
-  // <Ticket  ticket={[0,2,1,5,8]}/> */}
-  // <Lottery n={4} winningCond = {winningCond}  />
-  // </>
-  <TodoList/>
+  <>
+  {/* <Ticketnum num={5} />
+  <Ticketnum num={4} />
+  <Ticketnum num={3} /> */}
+  {/* <Ticket  ticket={[0,2,1]}/>
+  <Ticket  ticket={[0,2,1,5,8]}/> */}
+  <Lottery n={4} winningCond = {winningCond}  />
+  </>
+  // <TodoList/>
   );
 }
 export default App;
